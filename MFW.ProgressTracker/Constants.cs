133ad2be4bb7f7b72d09ca888204863fa686faab
MFW2000 <marijnwelmerink@gmail.com>
2024-12-08 22:59:04 +0100

    Clean project

---
+++ MFW.ProgressTracker/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MFW.ProgressTracker;
+
+/// <summary>
+/// Provides common text used throughout the application.
+/// </summary>
+public static class Constants
+{
+    public const string ApplicationName = "Progress Tracker";
+}
