32952c3c3e0e1d435fe8b69883b53cc048de4b74
MFW2000 <marijnwelmerink@gmail.com>
2024-12-12 21:02:05 +0100

    Add basic front-end

--- MFW.ProgressTracker/Constants.cs
+++ MFW.ProgressTracker/Constants.cs
@@ -5,6 +5,16 @@
 /// </summary>
 public static class Constants
 {
+    // General
     public const string ApplicationName = "Progress Tracker";
     public const string TagLine = "Track your progress across various activities";
+
+    // Home page
+    public const string HomeTitle = "Home";
+
+    // About page
+    public const string AboutTitle = "About";
+
+    // Aria
+    public const string AriaNavigationBar = "Toggle navigation";
 }
