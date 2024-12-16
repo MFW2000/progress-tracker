c2ab0d7f799ea2bb89fe213be42b65fd12ef7bc2
MFW2000 <marijnwelmerink@gmail.com>
2024-12-16 21:00:23 +0100

    Add alert component

--- MFW.ProgressTracker/Constants.cs
+++ MFW.ProgressTracker/Constants.cs
@@ -9,12 +9,18 @@ public static class Constants
     public const string ApplicationName = "Progress Tracker";
     public const string TagLine = "Track your progress across various activities";
 
-    // Home page
+    // Home
     public const string HomeTitle = "Home";
 
-    // About page
+    // About
     public const string AboutTitle = "About";
+
+    // Error
+    public const string ErrorTitle = "Error";
+    public const string ErrorSubtitle = "An error occurred while processing your request";
+    public const string ErrorRequestIdHeading = "Request ID";
 
     // Aria
     public const string AriaNavigationBar = "Toggle navigation";
+    public const string AriaClose = "Close";
 }
